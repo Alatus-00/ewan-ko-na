fdbc8a75a3794f474f46b9ce575ea42683eed8e6
Alatus-00 <johanscedricapdua@gmail.com>
2024-07-17 03:06:09 +0800

    Update volunteer.php

--- phpotangina/volunteer.php
+++ phpotangina/volunteer.php
@@ -29,6 +29,10 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Volunteer at Animal Shelter</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css"
+      rel="stylesheet"
+    />
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
