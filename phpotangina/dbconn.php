e7985d89e5fc23dedfa7f02da26fdbf35be51a4d
Alatus-00 <johanscedricapdua@gmail.com>
2024-07-17 02:50:50 +0800

    Update dbconn.php

--- phpotangina/dbconn.php
+++ phpotangina/dbconn.php
@@ -2,7 +2,7 @@
         $db_host = "localhost";
         $db_user = "root";
         $db_pass = "";
-        $db_name = "reg_users";
+        $db_name = "finals";
 
         $conn = new mysqli($db_host,$db_user,$db_pass,$db_name);
 
@@ -10,7 +10,6 @@
         echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
         exit();
         }
-
         function sanitize($input){
                 return strip_tags($input);
         }
